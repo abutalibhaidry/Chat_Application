d8e46d2e6415febe8308fd8b831662afccfd78f2
abutalibhaidry <ahaidry110@gmail.com>
2025-07-02 20:12:47 +0530

    ready to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,9 +6,12 @@ import userRoutes from "./routes/userRoute.js";
 import chatRoutes from "./routes/chatRoute.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser  from "cookie-parser"
+import path from "path"
 
 const app = express();
 const PORT = process.env.PORT || 5002;
+
+const __dirname = path.resolve();
 
 app.use(cors({
   origin: "http://localhost:5173",
@@ -24,6 +27,14 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 const httpServer = app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
