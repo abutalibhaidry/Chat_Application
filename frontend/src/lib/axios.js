5609205745146f1c2f336c2ff796a2104e13bf04
abutalibhaidry <ahaidry110@gmail.com>
2025-07-04 18:13:09 +0530

    cookie set

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios'
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const axiosInstance =  axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true, // SEND COOKIES WITH THE REQUEST
-})
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
